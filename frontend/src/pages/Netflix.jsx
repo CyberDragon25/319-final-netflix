13965f3421906b540fd78062440670aacfb4dbf4
Shubham <bhattacharya.shubham123@gmail.com>
2024-04-30 16:41:09 -0500

    completed navbar on netflix home screen

--- frontend/src/pages/Netflix.jsx
+++ frontend/src/pages/Netflix.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+
+
 
 export default function Netflix() {
-  return <div>Netflix</div>;
+
+  const [isscrolled, setIsscrolled] = useState(false);
+
+  window.onscroll = () => {
+    setIsscrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+  return (
+    <div>
+     <Navbar isscrolled={isscrolled} />
+    </div>
+
+  );
 }
